967a14c5a9858e5347e56504cfe27c1cf6d4bebc
shrr9 <pijahn@thphys.uni-heidelberg.de>
2022-02-11 20:57:22 +0100

    move esc caps

--- movefiles.sh
+++ movefiles.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+
+#put swap esc caps into xinitrc
+mkdir .config/swap_esc_caps
+cp swap_esc_caps.sh ~/.config/swap_esc_caps/swap_esc_caps.sh
+cp swap_esc_caps_script.sh ~/.config/swap_esc_caps/swap_esc_caps_script.sh
+chmod +x ~.config/swap_esc_caps/swap_esc_caps_script.sh
+x =`./~/.config/swap_esc_caps/swap_esc_caps_script.sh ; cat .xinitrc`
+echo "$x" > .xinitrc
+
+
+#nvim config
 mkdir ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
